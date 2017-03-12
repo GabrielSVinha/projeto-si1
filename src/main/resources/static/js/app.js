c047bbfae944ff1ee2d658c9e06ea2518ce894bf
Lucas Cordeiro <ecdb.lucas@gmail.com>
2017-03-12 19:53:27 -0300

    documentation on routes

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,6 +2,10 @@ var app = angular.module('ad-extreme', ['ui.router', 'ngMessages'])
     .config(['$locationProvider', '$stateProvider', function($locationProvider, $stateProvider) {
         $locationProvider.html5Mode(true);
 
+        /**
+         * If you're going to add another route, be sure to also add
+         * it in the class br.edu.ufcg.computacao.si1.config.WebConfig
+         */
         $stateProvider
             .state('home', {
                 url: '/',
